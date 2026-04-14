91020eeab56ec756db855423eb11f9c13397735b
Patarapong Limvipaveeanan <patarapong_li@minor.com>
2026-04-15 02:31:19 +0700

    feat: Update database connection settings: reduce connection limit and enable SSL verification

--- db/db.ts
+++ db/db.ts
@@ -4,15 +4,15 @@ import mysql from 'mysql2/promise';
 
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 1,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
   waitForConnections: true,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  // }
+  ssl: {
+    rejectUnauthorized: true,
+  }
 });
 
 export default pool;
