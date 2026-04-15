c9863415c344c991b2b2d12de2df2ba45bf2d026
Patarapong Limvipaveeanan <patarapong_li@minor.com>
2026-04-15 12:47:47 +0700

    feat: Increase database connection limit from 1 to 10 for improved performance

--- db/db.ts
+++ db/db.ts
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 
 
 const pool = mysql.createPool({
-  connectionLimit: 1,
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
